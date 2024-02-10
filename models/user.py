1016b1f380ae840252e03081c1b935b9a904b650
Ahmed <ahmed_l2@yahoo.com>
2024-02-10 19:34:28 +0300

    .

--- models/user.py
+++ models/user.py
@@ -5,7 +5,15 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """class User that inherits from BaseModel"""
+    """
+    User class that represents a user in the system.
+
+    Attributes:
+        email (str): The email address of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
 
     email = ""
     password = ""
